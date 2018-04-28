54750dc0de6d9395f162c1d2b6e9ac683b5e55e9
houyanping <houyanping@modo.cn>
2018-04-28 18:55:12 +0800

    增加打字机特效

--- src/pili/CustomEffect.ts
+++ src/pili/CustomEffect.ts
@@ -1,5 +1,15 @@
 module pili {
+	/**
+	 * 用例：pili.CustomEffect.instance.typerEffect(aaa, "这是一个字符串");
+	 * pili.CustomEffect.instance.destroyTyperEffect();
+	 */
 	export class CustomEffect {
+		private static _instance:pili.CustomEffect;
+		private arrTyperTimeId: number[];
+		public static get instance():pili.CustomEffect{
+			if(!this._instance) this._instance = new pili.CustomEffect();
+			return this._instance;
+		}
 		/**
 		* 文字打字机效果
 		* obj 文本对象
@@ -7,7 +17,8 @@ module pili {
 		* interval 打字间隔 毫秒
 		* backFun 打字动画完成回调
 		*/
-		private typerEffect(obj, content: string = "", interval: number = 200, backFun: Function = null): void {
+		public typerEffect(obj, content: string = "", interval: number = 200, backFun: Function = null): void {
+			this.arrTyperTimeId = [];
 			let strArr: Array<any> = content.split("");
 			let len: number = strArr.length;
 			for (let i = 0; i < len; i++) {
@@ -18,6 +29,20 @@ module pili {
 						backFun();
 					}
 				}, i, interval * i);
+				this.arrTyperTimeId.push(timeId);
+			}
+		}
+		
+		/**销毁打字机效果 */
+		public destroyTyperEffect() {
+			if (this.arrTyperTimeId) {
+				for (let i: number = 0, len: number = this.arrTyperTimeId.length; i < len; i++) {
+					if (this.arrTyperTimeId[i]) {
+						egret.clearTimeout(this.arrTyperTimeId[i]);
+					}
+				}
+				this.arrTyperTimeId.length = 0;
+				this.arrTyperTimeId = null;
 			}
 		}
 	}
